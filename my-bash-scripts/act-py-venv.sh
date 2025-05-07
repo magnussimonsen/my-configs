c7338b5a0d5b815351c7f9681519ef392cd7c13a
Magnus Simonsen <magnus.simonsen@protonmail.com>
2025-05-08 00:16:22 +0200

    Backup config files

--- my-bash-scripts/act-py-venv.sh
+++ my-bash-scripts/act-py-venv.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Path to your virtual environment
+# Path to python virtual environment
 VENV_PATH="/home/magnus/pyvenv"
 
 # Activate the virtual environment
 source "$VENV_PATH/bin/activate"
 
-# Optional: You can run a Python script or any command here after activation
-# python your_script.py
